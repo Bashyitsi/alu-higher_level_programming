8e4cd6756537cc896c23b0d7bd4cf04dad8844f7
Your Name <you@example.com>
2023-02-06 13:08:39 -0800

    Task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,8 +7,8 @@ def fizzbuzz():
 
     For multiples of three, print Fizz instead of the number.
     For multiples of five, print Buzz instead of the number.
-    For multiples of three and five, print FizzBuzz instead of the number. """
-    
+    For multiples of three and five, print FizzBuzz instead of the number.
+    """
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
@@ -16,7 +16,5 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif number % 5 == 0:
             print("Buzz ", end="")
-        else:
-            print("{} ".format(number), end="")
         else:
             print("{} ".format(number), end="")
