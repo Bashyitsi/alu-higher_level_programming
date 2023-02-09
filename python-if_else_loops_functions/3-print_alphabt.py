611d4ead476ad410ea3600277a591e26c6513421
Your Name <you@example.com>
2023-02-09 11:32:40 -0800

    Task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for a in range(97, 97 + 26):
-    if a == 113 or a == 101:
-       continue
-    print("{:c}".format(a), end='')
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print('{}'.format(chr(i)), end="")
