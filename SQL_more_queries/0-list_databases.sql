2747184d814a29b32dcf7bbc42e3b8be2657bccd
Your Name <you@example.com>
2023-03-31 12:51:43 -0700

    TASK  SQL

---
+++ SQL_more_queries/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- A script that lists all databases of your MySQL server
+-- Query to display all databases
+SHOW DATABASES;
---
+++ SQL_more_queries/5-full_table.sql
@@ -0,0 +1,3 @@
+-- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Query to describe a table
+SHOW CREATE TABLE first_table;
